29a09b849da6ba56a26e70f2fa6537115a4db212
Matthew Jagiello <mjagiello@gmail.com>
2020-01-16 21:29:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number = 0.0
 second_number = 1.0
 sum = first_number + second_number
 difference = first_number - second_number
-product = first_number * second_number
+product = first_number / second_number
 quotient = first_number / second_number
